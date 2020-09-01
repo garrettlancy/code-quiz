3e8c3a13e977bd042884058a9b6fe344ea308fc6
garrettlancy <glancy@live.com>
2020-09-01 03:30:56 -0400

    Finished css styling and added on hover effect for images.

--- code-quiz.js
+++ code-quiz.js
@@ -1,4 +1,13 @@
-jQuery(document).ready(function($){
+jQuery(document).ready(function ($) {
 
+    $("a").mouseenter(function () {
+        $(this).find("img").finish().animate({
+            opacity: "0.5"
+        });
+    }).mouseleave(function () {
+        $(this).find("img").finish().animate({
+            opacity: "1"
+        });
+    });
 
 });
